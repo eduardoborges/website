450cb5f31ee1a04c01aa19519bf4f753c5e02e9a
Eduardo Borges <euduardoborges@gmail.com>
2019-08-15 17:45:32 -0300

    novo site

---
+++ precache-manifest.5102d3dc7d2a91741c80ae17511cb17b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e5a85529725827c9346217d8833eb07",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f4cc5433bff3b3ec6177",
+    "url": "/static/css/main.256cc384.chunk.css"
+  },
+  {
+    "revision": "b59addaa24774fd2bf0e",
+    "url": "/static/js/2.15f94209.chunk.js"
+  },
+  {
+    "revision": "f4cc5433bff3b3ec6177",
+    "url": "/static/js/main.9bcba9d1.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "3e403e7ab1892d4524eb43e0f1476666",
+    "url": "/static/media/me1.3e403e7a.jpg"
+  }
+]);
